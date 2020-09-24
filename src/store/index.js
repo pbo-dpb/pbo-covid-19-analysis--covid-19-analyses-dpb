0bd3aa5800958aa1db33ba2a85a227145c668f04
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2020-09-24 01:00:52 +0000

    Removed Cost from most of the EFO's costings.

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,8 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state: {
-        //measures: require("./data/measures.json")
         measures: require.context('./data/measures/', true, /\.json$/i).keys().map(key => require("./data/measures/" + key.split('/').pop().split('.')[0])),
+        outlooks: require.context('./data/outlooks/', true, /\.json$/i).keys().map(key => require("./data/outlooks/" + key.split('/').pop().split('.')[0])),
     },
 
 })
