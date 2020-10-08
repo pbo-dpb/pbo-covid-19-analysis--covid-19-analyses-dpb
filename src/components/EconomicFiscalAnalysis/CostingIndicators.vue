1c827a4d288ecc84f4874fec67f18783649e18f3
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2020-10-08 01:44:54 +0000

    Attempted to make the economic and fiscal indicators fit when more years are added.

--- src/components/EconomicFiscalAnalysis/CostingIndicators.vue
+++ src/components/EconomicFiscalAnalysis/CostingIndicators.vue
@@ -1,35 +1,38 @@
 <template>
-  <aside class="bg-blue-100 p-2 md:-mx-2 my-8 rounded-lg">
+  <aside class="bg-blue-100 p-2 my-8 rounded-lg">
     <div
-      class="col-span-2 md:col-span-6 text-center text-lg pb-2 mb-2 border-b border-blue-800 leading-none"
+      class="col-span-2 text-center text-xl pb-2 mb-2 border-b border-blue-800 leading-none"
     >
       <component :is="'div'" class="font-bold text-blue-800">{{
         $root.strings.costingerp.indicators.header_title
       }}</component>
-      <span class="text-sm font-thin">{{
+      <!--<span class="text-sm font-thin">{{
         $root.strings.costingerp.indicators.header_note
-      }}</span>
+      }}</span>-->
     </div>
-
-    <li
-      v-for="(cost, indicator) in efa.indicators"
-      :key="indicator"
-      class="grid grid-2 md:grid-cols-8 gap-2 py-2 md:py-1 items-center"
-    >
-      <div class="col-span-2 md:col-span-6 text-center md:text-left">
-        <component :is="'span'">
-          {{ $root.strings.efas.indicators[indicator] }}</component
-        >
-      </div>
-      <div
-        v-for="(number, year) in cost"
-        :key="year + indicator"
-        class="col-span-1 text-center"
+    <ul class="divide-y divide-gray-400">
+      <li
+        v-for="(cost, indicator) in efa.indicators"
+        :key="indicator"
+        class="grid grid-2 gap-2 py-2 items-center"
       >
-        <div class="md:hidden font-thin text-sm text-gray-700">{{ year }}</div>
-        {{ number.toLocaleString($root.language) }}
-      </div>
-    </li>
+        <div class="col-span-2 text-center text-lg pb-2 text-blue-900">
+          <component :is="'span'">
+            {{ $root.strings.efas.indicators[indicator] }}</component
+          >
+        </div>
+        <div
+          v-for="(number, year) in cost"
+          :key="year + indicator"
+          class="col-span-1 text-center"
+        >
+          <div class="font-thin text-sm text-gray-700">{{ year }}</div>
+          <span class="text-blue-900 font-bold">{{
+            number.toLocaleString($root.language)
+          }}</span>
+        </div>
+      </li>
+    </ul>
   </aside>
 </template>
 <script>
