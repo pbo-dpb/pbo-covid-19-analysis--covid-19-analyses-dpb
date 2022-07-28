d529097c4ec8e10a443dd915101fd496dd7b5d54
root <remy.vanherweghem@parl.gc.ca>
2022-07-28 19:23:30 +0000

    Added missing file.

---
+++ src/components/TopHeader.vue
@@ -0,0 +1,24 @@
+<template>
+
+        <h1
+              class="text-4xl leading-none font-thin mb-4"
+            >
+              <router-link
+                :to="{name:'AnalysisHome'}"
+                >{{ title }}</router-link
+              >
+            </h1>
+
+
+</template>
+<script>
+export default {
+
+  computed: {
+
+    title() {
+      return this.$root.strings.title;
+    },
+  },
+};
+</script>
