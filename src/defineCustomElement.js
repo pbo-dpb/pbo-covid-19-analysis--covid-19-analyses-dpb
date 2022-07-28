f47986888666ed0b92dc57b2db83eb86cf1d2d7c
root <remy.vanherweghem@parl.gc.ca>
2022-07-28 18:48:26 +0000

    Started reintroducing some of the legacy payload content.

--- src/defineCustomElement.js
+++ src/defineCustomElement.js
@@ -1,18 +1,22 @@
 // Adapted from https://stackoverflow.com/questions/69808113/how-to-use-vue-router-and-vuex-inside-custom-element-from-root
 import { defineCustomElement as vueDefineCustomElement, h, createApp, getCurrentInstance } from 'vue'
+import collect from "collect.js"
 import strings from "./strings"
+import Costing from "./store/legacyModels/Costing.js"
+import Update from "./store/legacyModels/Update.js"
 import MarkdownContent from "./components/MarkdownContent.vue"
+import legacyPayload from './store/data/legacyPayload.json'
 const language = document.documentElement.lang;
 
 export const defineCustomElement = (component, { plugins = [] }) =>
-vueDefineCustomElement({
+  vueDefineCustomElement({
     render: () => h(component),
     styles: component.styles,
     props: {
       publicPath: String,
     },
     setup() {
-      const app = createApp()
+      const app = createApp();
       /**
        * Register global components
        */
@@ -25,16 +29,43 @@ vueDefineCustomElement({
       const inst = getCurrentInstance()
       Object.assign(inst.appContext, app._context)
       Object.assign(inst.provides, app._context.provides)
-      
+
+
+
+
       return {
         language,
+        payload: null
       }
     },
 
-  computed: {
-    strings() {
-      return strings[this.language];
+    computed: {
+      strings() {
+        return strings[this.language];
+      }
+    },
+
+    mounted() {
+      /**
+         * Setup hardcoded legacy payload content
+         */
+
+      let hcpl = legacyPayload;
+      hcpl.costings = collect(legacyPayload.costings).map((costing) => {
+        return new Costing(costing);
+      });
+      hcpl.updates = collect(legacyPayload.updates).map((update) => {
+        return new Update(update);
+      });
+      this.payload = hcpl;
+
+      this.initializeSearch();
+
+    },
+    methods: {
+      initializeSearch() {
+
+      }
     }
-  }
-    
+
   })
